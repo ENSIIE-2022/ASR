787e0414f77b495c9155a9d73abcdb4af6a74f86
Tchebychev <lucas.naveteur@hotmail.fr>
2019-10-07 15:28:21 +0200

    Exo 1 terminé

--- TP3/01-shellscript.sh
+++ TP3/01-shellscript.sh
@@ -3,12 +3,13 @@
 #Liste les noms des fichiers de type shellscript dans une arborescence donnée
 #et affiche le nombre de lignes de chaque fichiers
 
-path="/usr/"
+path="./"
 bash_regex="#!/bin/bash"
 for filename in `ls -p $path | grep -v /`;
 do
     line=$(head -n 1 "$path$filename")
+    number_of_line=$(wc -l "$path$filename" | cut -d' ' -f1)
     if [ $line == $bash_regex ]; then
-        echo "$filename is a script"
+        echo "$filename is a script with $number_of_line lignes (Does not count line with only \n)"
     fi
 done
