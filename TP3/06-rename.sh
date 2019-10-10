6fef1e903ec8df7827d3d22b394ff6852ff3c2b8
Tchebychev <lucas.naveteur@hotmail.fr>
2019-10-10 11:30:51 +0200

    TP3 - 06 terminé

--- TP3/06-rename.sh
+++ TP3/06-rename.sh
@@ -10,28 +10,31 @@ step1 () {
 }
 
 step2 () {
-    echo "Le fichier $1 doit-il être renommé en $2 ? (*/RETURN)"
-    read response
-    if [ ! $response = "RETURN" ]; then
-        #On souhaite renommer le fichier source $1
-        if [ $1 = $2 ]; then
-            echo "Le fichier destination est le même que le fichier source, arrêt du programme"
-        else
-            if [ -f $2 ]; then
-                echo "Vous allez écraser le fichier $2 par le fichier $1, souhaitez-vous continuer ? (Y/N)"
-                read replace
-                if [ $replace = "Y" ]; then
-                    mv $1 $2
+    for file in "$@"
+        do
+            echo "En quoi le fichier $file doit-être renommé (chemin complet) ?(*/RETURN)"
+            read response
+            if [ ! $response = "RETURN" ]; then
+                #On souhaite renommer le fichier source $file
+                if [ $file = $response ]; then
+                    echo "Le fichier destination est le même que le fichier source, arrêt du programme"
                 else
-                    echo "Annulation de modification, arrêt du programme..."
+                    if [ -f $response ]; then
+                        echo "Vous allez écraser le fichier $response par le fichier $file, souhaitez-vous continuer ? (Y/N)"
+                        read replace
+                        if [ $replace = "Y" ]; then
+                            mv $file $response
+                        else
+                            echo "Annulation de modification, arrêt du programme..."
+                        fi
+                    else
+                        mv $file $response
+                    fi
                 fi
             else
-                mv $1 $2
+                echo "Annulation de modification, arrêt du programme"
             fi
-        fi
-    else
-        echo "Annulation de modification, arrêt du programme"
-    fi
+        done
 }
 
 step1 $1
