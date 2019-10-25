669bd8afe6dac64614387c77f942f4905939f2cd
Tchebychev <lucas.naveteur@hotmail.fr>
2019-10-25 15:49:15 +0200

    TP3 - exo 11 : Ajout de l'aide

--- TP3/11-notes.sh
+++ TP3/11-notes.sh
@@ -13,7 +13,20 @@ while getopts "hl:d:anqp" option
     do
         case $option in
             h)
-                echo "--------------- Aide en ligne ----------------";;
+                echo "--------------- Aide en ligne ----------------"
+                echo "   => aide en ligne : liste des commandes avec leur syntaxe (h)"
+                echo "   => affichage de la note courante (l) ou de la note x (lx)"
+                echo "   => affichage de la note suivante (+ ou Return)"
+                echo "   => affichage de la note précédente (-)"
+                echo "   => destruction de la note courante (d) ou de la note x (dx)"
+                echo "   => ajout d'une note en fin de calepin (a)"
+                echo "   => affichage du nombre de notes du calepin (n)"
+                echo "   => sortie du programme (q ou Ctrl D)"
+                echo "   => affichage du calepin complet (p)"
+                echo "   => traitement des notes par contenu (l/exp/ ou d/exp/)"
+                echo "   => traitement des notes par intervalles (lx-y ou dx-y)"
+                echo "   => etc (selon vos désirs)"
+                ;;
             l)
                 echo "--------------- Affichage note ----------------"
                 echo `grep ^${OPTARG} $filename`
@@ -22,6 +35,7 @@ while getopts "hl:d:anqp" option
                 echo "--------------- Destruction note ----------------"
                 echo "removing the note number ${OPTARG}"
                 sed '/^${OPTARG}/ d $filename'
+                #Réarranger les entier de note après suppression
                 ;;
             a)
                 echo "--------------- Ajout de note ----------------"
